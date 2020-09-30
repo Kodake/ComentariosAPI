677ec77e7ea34c5ffc6db43539fd3b7bf614bec8
DESKTOP-O5BLINM\Admin <rflorentino@wepsys.net>
2020-09-29 23:44:07 -0400

    All changes

---
+++ BackEnd/BackEnd/Controllers/ComentarioController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentarioController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ComentarioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ComentarioController>
+        [HttpGet]
+        public ActionResult<List<Comentario>> Get()
+        {
+            try
+            {
+                var listComentario = _context.Comentario.ToList();
+                return Ok(listComentario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET api/<ComentarioController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Comentario> Get(int id)
+        {
+            try
+            {
+                var comentario = _context.Comentario.Find(id);
+                if (comentario == null)
+                {
+                    return NotFound();
+                }
+                return Ok(comentario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<ComentarioController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Comentario comentario)
+        {
+            try
+            {
+                _context.Add(comentario);
+                _context.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<ComentarioController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Comentario comentario)
+        {
+            try
+            {
+                if (id != comentario.Id)
+                {
+                    return BadRequest();
+                }
+
+                _context.Entry(comentario).State = EntityState.Modified;
+                _context.Update(comentario);
+                _context.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<ComentarioController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var comentario = _context.Comentario.Find(id);
+                if (comentario == null)
+                {
+                    return NotFound();
+                }
+                _context.Remove(comentario);
+                _context.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ BackEnd/BackEnd/Migrations/20200918025422_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comentario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(nullable: false),
+                    Creador = table.Column<string>(nullable: false),
+                    Texto = table.Column<string>(nullable: false),
+                    FechaCreacion = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentario");
+        }
+    }
+}
